feed210195351cb86aaf4615e778d5d3df119c17
AbdullahAnsari103 <abdullahansari01618@gmail.com>
2025-09-30 00:39:52 +0530

    Update api/index.js with Vercel serverless function entry point

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // Vercel serverless function entry point for AttendPro
 const { app } = require('../index.js');
 
-// Export the Express app as a serverless function
-module.exports = app;
+// Export the Express app as a serverless function handler for Vercel
+module.exports = (req, res) => app(req, res);
